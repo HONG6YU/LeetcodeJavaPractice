af0ccd4341859cfb73e896a4a5e98932beb73a97
SimonF <laissezfu@gmail.com>
2021-01-06 09:26:14 +0800

    Another change to leet840

--- leet840.java
+++ leet840.java
@@ -5,25 +5,19 @@ public class leet840 {
     List<List<Integer>> ans = new ArrayList<>();
     public List<List<Integer>> largeGroupPositions(String s) {
         int length = s.length();
+        int count = 1;
         for (int i = 0; i < length; i++) {
-            char c = s.charAt(i);
-            int curLength = 0;
-            int j = i;
-            for (; j < length; j++) {
-                char c2 = s.charAt(j);
-                if (c2==c) {
-                    curLength++;
-                }else{
-                    if (curLength>=3) {
-                        List<Integer> curAns = new ArrayList<>();
-                        curAns.add(i);
-                        curAns.add(j-1);
-                        ans.add(curAns);
-                    }
-                    break;
-                }
+            if (s.charAt(i)==s.charAt(i+1)) {
+                count++;
+                continue;
             }
-            i = j+1;
+            if (count>=3) {
+                List<Integer> curIntegers = new ArrayList<>();
+                curIntegers.add(i-count+1);
+                curIntegers.add(i);
+                ans.add(curIntegers);
+            }
+            count = 1;
         }
         return ans;
     }
